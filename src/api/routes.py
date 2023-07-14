045fb1e18968d98bdfcccc595a4f2a083d616ae9
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-07-14 03:51:29 +0000

    endpoint actualizado

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+import hashlib
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -22,48 +23,46 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+
 @api.route('/registro', methods=['POST'])
 def new_user():
     body = request.json  # lo que viene del request como un dic de python 🦎
     try:
         new_user = User(body['username'], body['email'],
-                          body['password'])
+                        body['password'])
         print(new_user)
         db.session.add(new_user)
         db.session.commit()
         return jsonify(new_user.serialize()), 200
     except Exception as err:
         return jsonify({"message": "Ah ocurrido un error inesperado ‼️" + str(err)}), 500
-    
+
 @api.route('/login', methods=['POST'])
 def login():
     username = request.json.get("username", None)
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    # verifys payload body
-    if username == None or password == None:
-        return jsonify({"msg": "Bad username or password ⛔️"}), 401
-    # search user existance into the DB
-    search_user = User.query.filter_by(username=username).one_or_none()
-    if search_user == None:
-        return jsonify({"message": "user not found "}), 404
-    # verifys payload body
-    if email == None or password == None:
-        return jsonify({"msg": "Bad email or password ⛔️"}), 401
-    # search user existance into the DB
-    search_user = User.query.filter_by(email=email).one_or_none()
-    if search_user == None:
-        return jsonify({"message": "user not found "}), 404
-    # verify thats the password is correct
-    # password saved as hash // with the password arriving hashed
-    # if search_user.password == hashlib.md5(password.encode('utf-8') ).hexdigest():
-    #     return jsonify({ "token" : create_access_token(identity=search_user.email) }), 200
 
-    if search_user.password == password:
+    # Verifica el cuerpo de la solicitud
+    if username is None or password is None or email is None:
+        return jsonify({"msg": "Bad username, email, or password ⛔️"}), 401
+
+    # Busca usuario existente por nombre de usuario
+    search_user = User.query.filter_by(username=username).first()
+    if search_user is None:
+        # Busca usuario existente por correo electrónico
+        search_user = User.query.filter_by(email=email).first()
+        if search_user is None:
+            return jsonify({"message": "User not found "}), 404
+
+    # Verifica que la contraseña sea correcta
+    password_hash = hashlib.md5(password.encode('utf-8')).hexdigest()
+    if search_user.password == password_hash:
         return jsonify({
-            "token": create_access_token(identity=search_user.username),
+            "token": create_access_token(identity=search_user.email),
             "user": search_user.username,
-            "idUser": search_user.id,
+            "idUser": search_user.id
         }), 200
-    # handling errors
-    return jsonify({"message": "password doesnt match, be carefull 🔓️ "}), 401
+
+    # Manejo de error para contraseña incorrecta
+    return jsonify({"message": "Incorrect password, please try again 🔓️"}), 401
