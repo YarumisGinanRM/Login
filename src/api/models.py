9ee7234cc4f50719e3b59da4c7798390794a422f
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-07-14 03:34:31 +0000

    endpoint agregados y models modificada

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    # is_active = db.Column(db.Boolean, unique=False, nullable=True)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.creation_date = date.today()
+        # self.is_active = True
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.username}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "creation_date": self.creation_date
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,11 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
+
 api = Blueprint('api', __name__)
 
 
@@ -16,3 +21,49 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/registro', methods=['POST'])
+def new_user():
+    body = request.json  # lo que viene del request como un dic de python 🦎
+    try:
+        new_user = User(body['username'], body['email'],
+                          body['password'])
+        print(new_user)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(new_user.serialize()), 200
+    except Exception as err:
+        return jsonify({"message": "Ah ocurrido un error inesperado ‼️" + str(err)}), 500
+    
+@api.route('/login', methods=['POST'])
+def login():
+    username = request.json.get("username", None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # verifys payload body
+    if username == None or password == None:
+        return jsonify({"msg": "Bad username or password ⛔️"}), 401
+    # search user existance into the DB
+    search_user = User.query.filter_by(username=username).one_or_none()
+    if search_user == None:
+        return jsonify({"message": "user not found "}), 404
+    # verifys payload body
+    if email == None or password == None:
+        return jsonify({"msg": "Bad email or password ⛔️"}), 401
+    # search user existance into the DB
+    search_user = User.query.filter_by(email=email).one_or_none()
+    if search_user == None:
+        return jsonify({"message": "user not found "}), 404
+    # verify thats the password is correct
+    # password saved as hash // with the password arriving hashed
+    # if search_user.password == hashlib.md5(password.encode('utf-8') ).hexdigest():
+    #     return jsonify({ "token" : create_access_token(identity=search_user.email) }), 200
+
+    if search_user.password == password:
+        return jsonify({
+            "token": create_access_token(identity=search_user.username),
+            "user": search_user.username,
+            "idUser": search_user.id,
+        }), 200
+    # handling errors
+    return jsonify({"message": "password doesnt match, be carefull 🔓️ "}), 401
