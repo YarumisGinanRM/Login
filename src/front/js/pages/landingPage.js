7e86649c49eaaacd8d57e6cf347339ac665eef1b
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-07-14 06:16:25 +0000

    page de bienvenida actualizada

--- src/front/js/pages/landingPage.js
+++ src/front/js/pages/landingPage.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "../../styles/landingPage.css";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
@@ -6,15 +6,17 @@ import { Link } from "react-router-dom";
 export const LandingPage = () => {
     const { store, actions } = useContext(Context);
     return (
-        <div>
-            <div>
-                <div className="landing-page">
-                    <div className="overlay d-flex align-items-center justify-content-center flex-column">
-                        <h1 className="mb-3">¡Bienvenido {store.user}!</h1>
-                        <i className="fa-solid fa-hand-holding-heart my-3"></i>
-                        <h4 className="mt-3 mb-2">Haz iniciado sesión correctamente</h4>
-                        <h4>¡Gracias por unirte!</h4>
-                    </div>
+        <div className="landing-page container-fluid p-0 m-0">
+            <div className="row d-flex justify-content-center align-items-center m-0">
+                <div className="col-md-8 col-lg-6 col-xl-4 text-center p-0 m-0">
+                    <h1 className="mb-3">¡Bienvenido {store.user}!</h1>
+                    <i className="fa-solid fa-hand-holding-heart text-primary my-3"></i>
+                    <h4 className="mt-3 mb-2">Haz iniciado sesión correctamente</h4>
+                    <h4>¡Gracias por unirte!</h4>
+                </div>
+                <div className="col-6 p-0">
+                    <img src="https://netmente.com/images/tab1.jpg"
+                        className="img-fluid" alt="Sample image" />
                 </div>
             </div>
         </div>
